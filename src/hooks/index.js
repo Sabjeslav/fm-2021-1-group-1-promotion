4788c9bf975f6ce24630daf35c679b5ad58ca927
DenisKostyukov <kostukov08@gmail.com>
2021-07-05 17:07:36 +0300

    delete useless hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,69 +1,39 @@
 import { useEffect, useReducer } from 'react';
-import {
-  userReducer,
-  userInitialState,
-  usersListReducer,
-  usersListInitialState,
-  tasksInitialState,
-  tasksReducer,
-} from '../reducers/index';
-import { actions } from '../reducers/actions';
-export function useUser (src, userId) {
-  const [user, userDispatch] = useReducer(userReducer, userInitialState);
-  useEffect(() => {
-    userDispatch({
-      type: actions.REQUEST,
-    });
-    fetch(src)
-      .then(res => res.json())
-      .then(data => {
-        const author = data.filter(item => item.id === userId);
-        userDispatch({
-          type: actions.SUCCESS,
-          data: author[0],
-        });
-      })
-      .catch(error => userDispatch({ type: actions.ERROR, error }));
-  }, []);
-  return { user, userDispatch };
-}
+import {actions} from '../reducers/actions'
 
-export function useUsers (src) {
-  const [users, usersDispatch] = useReducer(usersListReducer, usersListInitialState);
+const defaultInitialState = {
+  isFetching: false,
+  data: [],
+  error: null,
+};
+
+export function useReducerLoader (
+  reducer,
+  options = { payLoad: {}, link: null },
+  initialState = defaultInitialState
+) {
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
-    usersDispatch({
+    dispatch({
       type: actions.REQUEST,
+      payLoad: options.payLoad,
     });
-    fetch(src)
+    fetch(options.link)
       .then(res => res.json())
-      .then(data => {
-        usersDispatch({
+      .then(data =>
+        dispatch({
           type: actions.SUCCESS,
           data,
-        });
-      })
-      .catch(error => usersDispatch({ type: actions.ERROR, error }));
-  }, []);
-  return { users, usersDispatch };
+        })
+      )
+      .catch(error =>
+        dispatch({
+          type: actions.ERROR,
+          error,
+        })
+      );
+  }, [options.link]);
+
+  return [state, dispatch];
 }
 
-export function useTasks (src) {
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, tasksInitialState);
-
-  useEffect(() => {
-    tasksDispatch({
-      type: actions.REQUEST,
-    });
-    fetch(src)
-      .then(res => res.json())
-      .then(data => {
-        tasksDispatch({
-          type: actions.SUCCESS,
-          data,
-        });
-      })
-      .catch(error => tasksDispatch({ type: actions.ERROR, error }));
-  }, []);
-
-  return { tasks, tasksDispatch };
-}
